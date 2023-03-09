dcaa49250ffe3fe8062d75df5e429416ac98475f
JohannHei <JohannHei@users.noreply.github.com>
2023-03-09 14:54:37 +0000

    write result

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,24 +34,31 @@ def do_search_old_backend(query, search_header=[]):
 
     res = requests.post(endpoint, json={'query': graph_query,"operationName":"Search","variables":{"search":query}}, headers={"x-active-search-features":",".join(search_header)})
 
-    return json.loads(res.text)
+    return json.loads(res.text)["data"]["search"]["products"]
 
 
 search_input = st.text_input("Search Query")
 
 col1, col2 = st.columns(2)
 
+def write_result(products):
+    for p in products:
+        st.text(p["name"])
+        st.image(p["thumbnail"]["url"])
+        """
+        ------------
+        """
 
 with col1:
     """
     Status quo
     """
     search_result = do_search_old_backend(search_input)
-    st.write("You entered: ", search_result)
+    write_result(search_result)
 
 with col2:
     """
     is_search_boost_title
     """
     search_result = do_search_old_backend(search_input, ["is_search_boost_title"])
-    st.write("You entered: ", search_result)
+    write_result(search_result)
